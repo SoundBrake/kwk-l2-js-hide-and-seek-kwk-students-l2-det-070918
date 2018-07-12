5e858f81fe83f213b61557e78f2642027f84a2e1
SoundBrake <mesuper222@gmail.com>
2018-07-12 17:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function getFirstSelector(selection){
   return document.querySelector(selection)
 }
 
-function nestedTarget(nested){
-  return document.querySelector(nested).nestedTarget()
+function nestedTarget(){
+  return ("#nested .target")
 }
