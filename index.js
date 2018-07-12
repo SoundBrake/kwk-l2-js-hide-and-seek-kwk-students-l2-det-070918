09c5b726bd7d08fea536d8dd8ab595304333574e
SoundBrake <mesuper222@gmail.com>
2018-07-12 18:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
   for(let i = 0, l = rankedLists.length; i < l; i++){
     let children = rankedLists[i].children
     for(let j = 0, k = children.length; j< k; j++){
-      children[i].innerHTML = parseInt(children[j].innerHTML) + n
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
 }
